29d0a0c38c6ee97c7da800efcbcc77d0d3e91a56
Emils Matiss <emils.matiss@gmail.ca>
2017-03-16 11:14:40 -0400

    added OMP version

--- OMPSimpsons.c
+++ OMPSimpsons.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<omp.h>
 
+double simpsons(double a, double b, int n); 
+double f(double x);
+
+int main(int argc, char *argv[]) {
+	printf("Simpsons Rule.\n");
+	printf("%f\n", simpsons(1,10,4));
+	return 0;
+}
+
+double simpsons(double a, double b, int n) {
+	double h = (b - a) / n;
+	double result, p;
+	int i;
+	p = 0;
+
+    #pragma omp parallel for reduction (+:p)
+	for(i = 1; i < n; i++) {
+		if(i%2 == 0) {
+			p += f(a + i * h) * 2;
+		} else {
+			p += f(a + i * h) * 4;
+		}
+	}
+	#pragma omp single	
+	result = (h/3) * (f(a) + f(b) + p);
+	return result;
+}
+
+
+double f(double x) {
+	return x*x;
+}
