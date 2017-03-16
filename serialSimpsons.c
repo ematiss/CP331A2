bf8ae13a11021b82f50b70143ebe8fb43efaf26f
Emils Matiss <emils.matiss@gmail.ca>
2017-03-16 10:52:09 -0400

    Added serial implementation

--- serialSimpsons.c
+++ serialSimpsons.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
 
+double simpsons(double a, double b, int n); 
+double f(double x);
+
+int main(int argc, char *argv[]) {
+	printf("Simpsons Rule.\n");
+	printf("%f\n", simpsons(1,10,4));
+	return 0;
+}
+
+double simpsons(double a, double b, int n) {
+	double h = (b - a) / n;
+	double x, result, p;
+	int i;
+	p= 0;
+	x = a + h;
+	
+	for(i = 1; i < n; i++) {
+		if(i%2 == 0) {
+			p += f(x) * 2;
+		} else {
+			p += f(x) * 4;
+		}
+
+		x += h;
+	}
+		
+	result = (h/3) * (f(a) + f(b) + p);
+	return result;
+}
+
+
+double f(double x) {
+	return x*x;
+}
